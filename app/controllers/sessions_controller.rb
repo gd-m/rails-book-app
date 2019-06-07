f32f26bcd9bd305cd4350d3306830fbbfa4f9b25
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-06-06 21:54:23 -0500

    add session controller with methods welcome and home

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+	def welcome
+		if user_signed_in?
+			redirect_to home_path
+		else
+			render 'welcome'
+		end
+
+	end
+
+	def home
+		if user_signed_in?
+			@user = current_user
+			render 'home'
+		else
+			redirect_to root_path
+		end
+		
+	end
+end
