ce02a24c955730722011be5e95ba1cd5b1d3fd94
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-06-07 00:42:12 -0500

    created registration controller to override devise controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationsController < Devise::RegistrationsController
+	def new
+		super
+	end
+
+	def create
+		#costum logic
+	end
+
+	def update
+		super
+	end
+
+	
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,7 @@ class SessionsController < ApplicationController
 		else
 			redirect_to root_path
 		end
-		
 	end
+
+	
 end
